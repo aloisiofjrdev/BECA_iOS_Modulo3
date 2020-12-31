c700e258fe03943a926606655dcb3beb2497306c
aloisiofjrdev <aloisiofjr@hotmail.com>
2020-12-31 15:03:49 -0300

    Criando o menu opcoes

---
+++ Agenda/Componentes/MenuOpcoesAlunos.swift
@@ -0,0 +1,28 @@
+//
+//  MenuOpcoesAlunos.swift
+//  Agenda
+//
+//  Created by Aloisio Formento Junior on 31/12/20.
+//  Copyright © 2020 Alura. All rights reserved.
+//
+
+import UIKit
+
+enum MenuActionSheetAluno {
+    case sms
+}
+
+class MenuOpcoesAlunos: NSObject {
+    func configuraMenuDeOpcoesDoAluno(completion: @escaping(_ opcap: MenuActionSheetAluno) -> Void) -> UIAlertController {
+        let menu = UIAlertController(title: "Atenção", message: "escolha uma das opcoes abaixo", preferredStyle: .actionSheet)
+        
+        let sms = UIAlertAction(title: "Enviar SMS", style: .default) { (acao) in
+            completion(.sms)
+        }
+        menu.addAction(sms)
+        let cancelar = UIAlertAction(title: "cancelar", style: .cancel, handler: nil)
+        menu.addAction(cancelar)
+        return menu
+    }
+    
+}
